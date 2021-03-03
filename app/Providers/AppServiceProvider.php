92e0dd743cdef803bfd24ea5f4c1dae198550d37
Moin Dhattiwala <moin.dhattiwala@iflair.com>
2021-03-03 12:53:16 +0530

    Redirection to https added for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    {          
+        if (env('APP_ENV') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
     }
 }
